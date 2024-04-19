a2bf793e2ca1749843f4cc519b6bf6dd741d4b0c
Vaibhav17031996 <vaibhavpatwal3@gmail.com>
2024-04-19 23:31:12 +0530

    done

--- components/UsersList.js
+++ components/UsersList.js
@@ -1,11 +1,18 @@
-import { useMemo } from 'react';
+import { useMemo } from "react";
 
 function UsersList({ users, startingLetter }) {
-  
+  const filteredUsers = useMemo(() => {
+    if (!startingLetter) return users;
+    return users.filter(({ name }) => {
+      return name[0] === startingLetter;
+    });
+  }, [startingLetter]);
 
   return (
     <ul>
-      <li key={}>{}</li>
+      {filteredUsers.map((user) => {
+        return <li key={user.id}>{user.name}</li>;
+      })}
     </ul>
   );
 }
